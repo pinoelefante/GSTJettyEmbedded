12e9a090e29d7ba49d1bf40f337a90a190f6dbef
pinoelefante <pinoelefante@hotmail.it>
2014-07-30 14:21:08 +0200

    utorrent

--- src/gst/download/UTorrent.java
+++ src/gst/download/UTorrent.java
@@ -1,5 +1,9 @@
 package gst.download;
 
+import java.io.File;
+
+import gst.programma.OperazioniFile;
+import gst.programma.Settings;
 import gst.serieTV.Torrent;
 
 public class UTorrent implements BitTorrentClient{
@@ -32,6 +36,33 @@ public class UTorrent implements BitTorrentClient{
 	public boolean downloadTorrent(Torrent t, String path) {
 		// TODO Auto-generated method stub
 		return false;
+	}
+	public static String rilevaInstallazione(){
+		String path = null;
+		if(Settings.isWindows()){
+			path = System.getenv("APPDATA")+File.separator+"uTorrent"+File.separator+"uTorrent.exe";
+			if(OperazioniFile.fileExists(path))
+				return path;
+			if(Settings.is32bit()){
+				path = System.getenv("PROGRAMFILES")+File.separator+"uTorrent"+File.separator+"uTorrent.exe";
+				if(OperazioniFile.fileExists(path))
+					return path;
+			}
+			else {
+				path = System.getenv("PROGRAMFILES")+File.separator+"uTorrent"+File.separator+"uTorrent.exe";
+				if(OperazioniFile.fileExists(path))
+					return path;
+				path = System.getenv("PROGRAMFILES(x86)")+File.separator+"uTorrent"+File.separator+"uTorrent.exe";
+				if(OperazioniFile.fileExists(path))
+					return path;
+			}
+			path = Settings.getCurrentDir() + File.separator + "uTorrent.exe";
+		}
+		return null;
+	}
+	public static void main(String[] args){
+		String p = rilevaInstallazione();
+		System.out.println(p==null?"Percorso non trovato":p);
 	}
 
 }
