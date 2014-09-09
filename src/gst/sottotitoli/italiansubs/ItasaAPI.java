7b2ea9c60a4ce22893257f725e86b536b3bab04a
Pino <pinoelefante@hotmail.it>
2014-09-09 04:56:13 +0200

    download sub itasa

--- src/gst/sottotitoli/italiansubs/ItasaAPI.java
+++ src/gst/sottotitoli/italiansubs/ItasaAPI.java
@@ -1,33 +1,47 @@
 package gst.sottotitoli.italiansubs;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map.Entry;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
 import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.client.methods.RequestBuilder;
 import org.apache.http.cookie.Cookie;
 import org.apache.http.impl.client.BasicCookieStore;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.select.Elements;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 public class ItasaAPI {
-	private String AUTHCODE;
-	private String APIKEY;
+	private String AUTHCODE="";
+	private String APIKEY="87c9d52fba19ba856a883b1d3ddb14dd";
 	
 	private String API_SHOWLIST="https://api.italiansubs.net/api/rest/shows?apikey="+APIKEY;
 	private String API_SUB_GETID = "https://api.italiansubs.net/api/rest/subtitles/search?q=<QUERY>&show_id=<SHOW_ID>&version=<VERSIONE>&apikey="+APIKEY;
 	private String API_LOGIN="https://api.italiansubs.net/api/rest/users/login?username=<USERNAME>&password=<PASSWORD>&apikey="+APIKEY;
+	private String API_DOWNLOAD="https://api.italiansubs.net/api/rest/subtitles/download?subtitle_id=<ID_SUB>&authcode=<AUTHCODE>&apikey="+APIKEY;
 	
 	private CloseableHttpClient httpclient;
 	private BasicCookieStore cookieStore;
@@ -42,6 +56,7 @@ public class ItasaAPI {
 	public boolean login(String username, String password){
 		String auth=verificaLogin(username, password);
 		if(auth!=null){
+			AUTHCODE=auth;
 			try {
 				loginWeb(username,password);
 			}
@@ -56,14 +71,13 @@ public class ItasaAPI {
 	}
 	private void loginWeb(String username, String password) throws Exception{
 		HttpGet httpget = new HttpGet("http://www.italiansubs.net/index.php");
+		List<NameValuePair> parametri=null;
 		CloseableHttpResponse response1 = httpclient.execute(httpget);
         try {
             HttpEntity entity = response1.getEntity();
 
             System.out.println("Login form get: " + response1.getStatusLine());
-            EntityUtils.consume(entity);
             
-
             System.out.println("Initial set of cookies:");
             List<Cookie> cookies = cookieStore.getCookies();
             if (cookies.isEmpty()) {
@@ -74,9 +88,40 @@ public class ItasaAPI {
                     System.out.println("- " + cookies.get(i).toString());
                 }
             }
+            parametri = getParameters(EntityUtils.toString(entity));
+            parametri.add(new BasicNameValuePair("username",username));
+            parametri.add(new BasicNameValuePair("passwd",password));
         } 
         finally {
             response1.close();
+        }
+        
+        RequestBuilder rq=RequestBuilder.post();
+        for(int i=0;parametri!=null && i<parametri.size();i++){
+        	rq.addParameter(parametri.get(i));
+        }
+        
+        HttpUriRequest login = rq.setUri("http://www.italiansubs.net/index.php").build();
+        CloseableHttpResponse response2 = httpclient.execute(login);
+        try {
+            HttpEntity entity = response2.getEntity();
+
+            System.out.println("Login form get: " + response2.getStatusLine());
+            EntityUtils.consume(entity);
+
+            System.out.println("Post logon cookies:");
+            List<Cookie> cookies = cookieStore.getCookies();
+            if (cookies.isEmpty()) {
+                System.out.println("None");
+            } 
+            else {
+                for (int i = 0; i < cookies.size(); i++) {
+                    System.out.println("- " + cookies.get(i).toString());
+                }
+            }
+        } 
+        finally {
+            response2.close();
         }
 	}
 	public String verificaLogin(String username, String password){
@@ -98,5 +143,45 @@ public class ItasaAPI {
 			e.printStackTrace();
 		}
 		return null;
+	}
+	private List<NameValuePair> getParameters(String html){
+		List<NameValuePair> params=new ArrayList<NameValuePair>();
+		org.jsoup.nodes.Document doc=Jsoup.parse(html);
+		org.jsoup.nodes.Element form=doc.getElementById("form-login");
+		Elements input=form.getElementsByTag("input");
+		for(int i=0;i<input.size();i++){
+			org.jsoup.nodes.Element el=input.get(i);
+			if(el.attr("name").compareTo("username")==0 || el.attr("name").compareTo("passwd")==0)
+				continue;
+			params.add(new BasicNameValuePair(el.attr("name"), el.attr("value")));
+		}
+		return params;
+	}
+	public boolean download(int idSub, String folder) throws Exception{
+		String url=API_DOWNLOAD.replace("<ID_SUB>", ""+idSub).replace("<AUTHCODE>", AUTHCODE);
+		HttpGet req=new HttpGet(url);
+		CloseableHttpResponse response=httpclient.execute(req);
+		HttpEntity entity = response.getEntity();
+		InputStream content=entity.getContent();
+		new File(folder).mkdirs();
+		FileOutputStream fos=new FileOutputStream(folder+File.separator+idSub+".zip");
+		byte[] buff=new byte[1024];
+		int read=0;
+		while((read=content.read(buff))>0){
+			fos.write(buff,0, read);
+		}
+		content.close();
+		fos.close();
+		return true;
+	}
+	public static void main(String[] args){
+		ItasaAPI api=new ItasaAPI();
+		api.login("pinoelefante", "elefante");
+		try {
+			api.download(20738,"C:\\download");
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
