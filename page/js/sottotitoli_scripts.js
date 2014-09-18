5d932376477e40e6f1ac019fb907bb84949c162d
pinoelefante <pinoelefante@hotmail.it>
2014-09-19 01:40:52 +0200

    .

--- page/js/sottotitoli_scripts.js
+++ page/js/sottotitoli_scripts.js
@@ -1,3 +1,17 @@
+$(document).ready(function() {
+	loadSeriePreferite();
+	
+	var config = {
+		'.chosen-select'           : {},
+		'.chosen-select-deselect'  : {allow_single_deselect:true},
+		'.chosen-select-no-single' : {disable_search_threshold:10},
+		'.chosen-select-no-results': {no_results_text:'Oops, nothing found!'},
+		'.chosen-select-width'     : {width:"95%"}
+	}
+	for (var selector in config) {
+		$(selector).chosen(config[selector]);
+	}
+});
 function loadSeriePreferite() {
 	$.ajax({
 		type : "POST",
@@ -5,18 +19,33 @@ function loadSeriePreferite() {
 		data : "action=getSeriePreferite",
 		dataType : "xml",
 		success : function(msg) {
-			operazioneInCorso("Caricando le serie preferite");
 			$(msg).find("serie").each(function() {
 				var nome = $(this).find("name").text();
 				var id = $(this).find("id").text();
 				var provider = $(this).find("provider").text();
 				var provider_name = $(this).find("provider_name").text();
-				var elem = creaSerieElementoPagina("<b>"+nome+"</b> - "+provider_name, id, provider);
+				var itasa = $(this).find("id_itasa").text();
+				var subsfactory = $(this).find("id_subsfactory").text();
+				var subspedia = $(this).find("id_subspedia").text();
+				
+				var option = document.createElement("option");
+				option.value=id;
+				option.innerHTML=nome;
+				option.provider=provider;
+				option.itasa=itasa;
+				option.subsfactory=subsfactory;
+				option.subspedia=subspedia;
+				
+				$(option).appendTo("#seriePreferite");
 			});
+			$("#seriePreferite").trigger("chosen:updated");
+			$("#seriePreferite").trigger("change");
 		},
 		error : function(msg) {
-			operazioneInCorso("");
 			showModal("","Si è verificato un errore durante l'aggiornamento");
 		}
 	});
+}
+function onchangePreferita(val){
+	
 }
