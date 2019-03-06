d34a695aea745cb6ea91a847feea1fdadd85c775
pinoelefante <pinoelefante@hotmail.it>
2019-03-06 17:31:30 +0100

    added basic config

--- src/server/ServerStart.java
+++ src/server/ServerStart.java
@@ -3,20 +3,12 @@ package server;
 import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.handler.ContextHandlerCollection;
 
+import server.settings.SettingsController;
+
 public class ServerStart {
 	private static JettyServer jettyServer;
 	
 	public static void main(String[] args) {
-		/*
-		try {
-    		if(HttpOperations.GET_withBoolean("http://localhost:8585/OperazioniSistemaServlet?action=isOpen")){
-    			System.out.println("Un'altra istanza è in esecuzione");
-    			HttpOperations.GET_withBoolean("http://localhost:8585/OperazioniSistemaServlet?action=show");
-    			System.exit(0);
-    		}
-		}
-		catch(Exception e){}
-		*/
 		ContextHandlerCollection contexts = new ContextHandlerCollection();
 		contexts.setHandlers(new Handler[] { new AppContextBuilder().buildWebAppContext() });
 
@@ -27,22 +19,11 @@ public class ServerStart {
 		}
 		catch (Exception e1) {
 			e1.printStackTrace();
-			return;
+			System.exit(-1);;
 		}
 		
-		Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
-			public void run() {
-				System.out.println("Shutdown hook - main");
-				if(jettyServer.isStarted()) {
-					try {
-						jettyServer.stop();
-					} 
-					catch (Exception exception) {
-						exception.printStackTrace();
-					}
-				}
-				System.exit(0);
-			}
-		},"Stop Jetty Hook")); 
+		SettingsController.getInstance().setUTorrentPath("C:\\Users\\pinoe\\AppData\\Roaming\\uTorrent\\uTorrent.exe");
+		SettingsController.getInstance().setDownloadDir("D:\\SerieTV");
+		SettingsController.getInstance().setVLCPath("C:\\Program Files\\VideoLAN\\VLC\\vlc.exe");
 	}
 }
